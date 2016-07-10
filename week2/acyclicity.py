af2168753c2feb81bfab4621c91d55f3e6e68f13
Wei Lu <wlu@quantcast.com>
2016-07-10 11:09:55 +0800

    Revert "week 2 incorrect solution"
    This reverts commit 1ea8c989d8429f0dc19f4da14483bc64b0719f39.

--- week2/acyclicity.py
+++ week2/acyclicity.py
@@ -3,22 +3,25 @@
 import sys
 
 
-def explore(v, adj, visited, start):
+def explore(v, adj, visited, post_order, count):
     visited.add(v)
     for w in adj[v]:
         if w not in visited:
-            if explore(w, adj, visited, start):
-                return True
-        elif w == start:
-            return True
-    return False
+            explore(w, adj, visited, post_order, count)
+    post_order[v] = count[0]
+    count[0] += 1
 
 def acyclic(adj):
     visited = set()
-    start = 0
+    post_order = [0] * len(adj)
+    count = [0] # use an array because int references by value
     for v in range(0, len(adj)):
         if v not in visited:
-            if explore(v, adj, visited, start):
+            explore(v, adj, visited, post_order, count)
+    # v -> w, w should have a smaller post order for it to be a DAG
+    for v in range(0, len(adj)):
+        for w in adj[v]:
+            if post_order[v] < post_order[w]:
                 return 1
     return 0
 
